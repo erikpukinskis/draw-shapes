59d6a953047707403cc0038756803949b164cb91
Erik Pukinskis <erik@snowedin.net>
2017-07-18 17:18:46 -0700

    Save to a universe

--- boxes.js
+++ boxes.js
@@ -39,33 +39,41 @@ library.using(
           return rgb
         }
 
+        function paintSwatch(bounds, color) {
+
+          var el = element(
+            ".swatch.active",
+            element.style({
+            "background": color,
+            "width": (bounds.maxX - bounds.minX)+"px",
+            "height": (bounds.maxY - bounds.minY)+"px",
+            "left": bounds.minX+"px",
+            "top": bounds.minY+"px",
+          }))
+
+          el.assignId()
+
+          addHtml(el.html())
+
+          return el
+        }
+
+
         return {
 
           create: function(x,y) {
             this.bounds = {
-              minX: x,
-              minY: y,
-              maxX: x,
-              maxY: y,
+              minX: x-3,
+              minY: y-3,
+              maxX: x+2,
+              maxY: y+2,
             }
             this.lastX = x
             this.lastY = y
+            this.color = getColor(x,y)
 
-            var el = element(
-              ".swatch.active",
-              element.style({
-              "background": getColor(x,y),
-              "width": "5px",
-              "height": "5px",
-              "left": x+"px",
-              "top": y+"px",
-            }))
-
-            el.assignId()
+            var el = paintSwatch(this.bounds, this.color)
             this.id = el.id
-
-            addHtml(el.html())
-
           },
 
           smudge: function(x, y) {
@@ -93,10 +101,11 @@ library.using(
 
             this.lastX = x
             this.lastY = y
+            this.color = getColor(x,y)
 
             var node = document.getElementById(this.id)
             node.style.transition = "none"
-            node.style.background = getColor(x,y)
+            node.style.background = this.color
             update(this, node)
           }
 
@@ -119,17 +128,38 @@ library.using(
       }
     )
 
-    var up = bridge.defineFunction(
-      [swatches],
-      function up(swatches) {
-        document.getElementById(swatches.id).classList.remove("active")
-        swatches.id = null
+    var universe = bridge.defineSingleton(
+      "paintingUniverse",
+      [bridgeModule(lib, "tell-the-universe", bridge)],
+      function(tellTheUniverse) {
+        var node
+
+        var universe = tellTheUniverse.called("painting").withNames({
+          paintSwatch: "paint-swatch"})
+
+        universe.onStatement(function() {
+          var isFirst = !node
+          if (isFirst) {
+            node = document.querySelector(".universe")
+            node.innerHTML += "A wild universe appeared!"
+          }
+          node.innerHTML += " <div></div>"
+        })
+
+        return universe
       }
     )
 
-    var finger = element.style(".swatch, .canvas", {
-      "cursor": "pointer",
-    })
+    var up = bridge.defineFunction(
+      [swatches, universe],
+      function up(swatches, universe) {
+        document.getElementById(swatches.id).classList.remove("active")
+
+        universe.do("paintSwatch", swatches.bounds, swatches.color)
+
+        swatches.id = null
+      }
+    )
 
     var trace = element(
       "img.trace",
@@ -140,15 +170,6 @@ library.using(
         "position": "absolute",
       })
     )
-
-    var swatch = element.style(".swatch", {
-      "position": "absolute",
-      "opacity": "0.8",
-
-      ".active": {
-        "opacity": "1",
-      }
-    })
 
     var canvas = element(".canvas",
       element.style({
@@ -164,12 +185,39 @@ library.using(
       onmouseup: up.withArgs(bridge.event).evalable()}
     )
 
+    var fingerStyle = element.style(".swatch, .canvas", {
+      "cursor": "pointer",
+    })
+
+    var swatchStyle = element.style(".swatch", {
+      "position": "absolute",
+      "opacity": "0.8",
+
+      ".active": {
+        "opacity": "1",
+      }
+    })
+
+    var universeStyle = element.style(".universe", {
+      "background": "lightblue",
+      "padding": "5px",
+
+      " div": {
+        "display": "inline-block",
+        "width": "10px",
+        "height": "10px",
+        "background": "blue",
+      }
+    })
+
     var page = element([
-      element("p", element.style({"margin-top": "400px"}), "Fuck. I need to get out of this box. I need to stop using these drugs to push me along. The door. It's here. I know it. I can find it. I just have to reach...."),
+      element(".universe", element.style({"margin-top": "500px"})),
+
+      element("p", "Fuck. I need to get out of this box. I need to stop using these drugs to push me along. The door. It's here. I know it. I can find it. I just have to reach...."),
       element("Step 1: Touch the picture to pool colors and make a color palette"),
       trace,
       canvas,
-      element.stylesheet(finger, swatch),
+      element.stylesheet(fingerStyle, swatchStyle, universeStyle),
     ])
 
     host.onSite(function(site) {
