7dad5d5024101e6029c7652248e31f7378234668
Erik Pukinskis <erik@snowedin.net>
2017-07-31 15:46:46 -0700

    Fix demo

--- demo.js
+++ demo.js
@@ -1,36 +1,35 @@
 var library = require("module-library")(require)
 
 library.using(
-  [".", library.ref(), "browser-bridge", "bridge-module", "tell-the-universe", "express", "html-painting"],
-  function(paintOnPicture, lib,  BrowserBridge, bridgeModule, tellTheUniverse, express, htmlPainting) {
+  [".", library.ref(), "web-host", "bridge-module", "tell-the-universe", "express", "html-painting"],
+  function(paintOnPicture, lib,  host, bridgeModule, tellTheUniverse, express, htmlPainting) {
     
-    var app = express()
-    var bridge = new BrowserBridge()
+    host.onRequest(function(getBridge) {
+      var bridge = getBridge()
 
-    var universe = bridge.defineSingleton(
-      "paintingUniverse",
-      [bridgeModule(lib, "tell-the-universe", bridge), bridgeModule(lib, "html-painting", bridge)],
-      function(tellTheUniverse, htmlPainting) {
-        var node
+      var universe = bridge.defineSingleton(
+        "paintingUniverse",
+        [bridgeModule(lib, "tell-the-universe", bridge), bridgeModule(lib, "html-painting", bridge)],
+        function(tellTheUniverse, htmlPainting) {
+          var node
 
-        var universe = tellTheUniverse.called("paintings").withNames({
-          htmlPainting: htmlPainting})
+          var universe = tellTheUniverse.called("paintings").withNames({
+            htmlPainting: htmlPainting})
 
-        universe.mute()
+          universe.mute()
 
-        universe.onStatement(function(call, args) {
-          console.log("called function "+call+" in universe. Arguments are", args)
-        })
+          universe.onStatement(function(call, args) {
+            console.log("called function "+call+" in universe. Arguments are", args)
+          })
 
-        return universe
-      }
-    )
+          return universe
+        }
+      )
 
-    var painting = paintOnPicture(bridge, universe)
+      var painting = paintOnPicture(bridge, universe)
 
-    app.get("/", bridge.requestHandler(painting))
-
-    app.listen(2001)
+      bridge.send(painting)
+    })
 
   }
 )
